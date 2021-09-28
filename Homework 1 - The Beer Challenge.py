0fa52bc8086cd6ac22b4a80a3342385ff2658938
Carlton (Local) <cloanword@gmail.com>
2021-09-28 17:54:16 -0400

    Imported the Functions that he provides in "HWBeerFunctions" to the main file.

--- Homework 1 - The Beer Challenge.py
+++ Homework 1 - The Beer Challenge.py
@@ -2,6 +2,17 @@
 # Group Homework Project 1 - The Beer Challenge
 # Ryan Hatcher and Carlton Howard
 # Instructions: Create a Python Program that can keep track of the entire brewing process from start to finish.
+# TOD O: Outline the entire brewing Process
 # DUE 10.04.2021 for Class CGS2060 (Intro to Computer Programming)
 #
 # All Comments go to the line(s) below them
+
+# Importing the Functions he provides us
+import HWBeerFunctions
+HWBeerFunctions.millGrain()
+HWBeerFunctions.mashConversion()
+HWBeerFunctions.kettle()
+HWBeerFunctions.heatKettle()
+HWBeerFunctions.addHops()
+HWBeerFunctions.prepareYeast()
+HWBeerFunctions.startFermentataion()
