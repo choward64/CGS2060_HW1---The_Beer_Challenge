ee851d463b266c404db30461c123518896c48b9b
Carlton <carltonhoward0428@gmail.com>
2021-09-29 18:27:38 -0400

    working on mash added test environment 4
    Signed-off-by: Carlton <carltonhoward0428@gmail.com>

--- Test 4.py
+++ Test 4.py
@@ -1,5 +1,3 @@
-# Importing the Functions he provides us
-
 import HWBeerFunctions
 Options = open("HW_1_Data.txt", "r")
 lines = Options.readlines()
@@ -16,11 +14,3 @@ HWBeerFunctions.mashConversion(
     ourWorld=int(D_OurWorld+"1") # Takes the OurWorld value from ln01 of HW_Data.
 
 )
-
-
-
-# TO-DO: Read HW_1_Data and define the variables that the above functions require
-
-# Opens the data file that the functions are going to pull from
-
-Options.close()
