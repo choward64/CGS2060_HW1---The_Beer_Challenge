94821b550887e83c9779ac68e6db4530886de69e
Ryan <haxhatcher@gmail.com>
2021-10-01 16:05:52 -0400

    Kettle is done, might have to recheck -R heatkettle almost finished

--- test 7.py
+++ test 7.py
@@ -1,1 +1,20 @@
-#Doing heat kettle in the morning :)
+
+
+
+import HWBeerFunctions
+HWBeerFunctions.heatKettle()
+
+infile = open("HW_1_Data.txt", "r")
+
+WORLD = 0
+
+try:
+    heated = kettle(1, WORLD)
+    if heated == 0:
+        raise ValueError
+    else:
+        print("Ketle was heated.")
+except ValueError:
+    Print("something went wrong")
+    exit(-1)
+
