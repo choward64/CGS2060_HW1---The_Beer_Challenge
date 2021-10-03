382705a13c663ef94e68e58845cab520a0a8bd98
Carlton (Local) <cloanword@gmail.com>
2021-10-03 18:16:10 -0400

    Stuck on the error in Test file 10 on line 21
    Signed-off-by: Carlton <carltonhoward0428@gmail.com>

---
+++ Test 10.py
@@ -0,0 +1,60 @@
+
+import HW1BeerFunctions_New
+Options = open("HW_1_Data.txt", "r")
+Gallons_Of_Beer = int(Options.readline().strip())
+Ratio_Of_Malt = int(Options.readline().strip())
+Pounds_Of_Hops = int(Options.readline().strip())
+Grams_Of_Yeast = int(Options.readline().strip())
+
+WORLD = 0
+
+millGrain = HW1BeerFunctions_New.millGrain(
+    barleyQuantity = Gallons_Of_Beer * Ratio_Of_Malt,
+    ourWorld = int(WORLD),
+
+)
+
+barleyQuantity = Gallons_Of_Beer * Ratio_Of_Malt
+Pounds_Of_Malt = 0
+
+while True:
+    Pounds_Of_Malt = millGrain(barleyQuantity, WORLD)
+    if Pounds_Of_Malt == 0:
+        print("The Milling Machine Was Broken. Trying Again...")
+    else:
+        break
+mashConversion = HW1BeerFunctions_New.mashConversion(
+   # ourWorld = int(WORLD),
+   # action = 0,
+)
+worked = 0
+
+try:
+    worked = mashConversion(0, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been filled!")
+except ValueError:
+    print("Oops, Something Went Wrong!")
+    exit(-1)
+
+try:
+    worked = mashConversion(1, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been heated.")
+except ValueError:
+    print("Oops, Something went wrong!")
+    exit(-1)
+
+try:
+    worked = mashConversion(2, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been loaded!")
+except ValueError:
+    print("Oops, Something went wrong!")
+    exit(-1)
--- Test 5.py
+++ Test 5.py
@@ -13,8 +13,9 @@ print("Value of lines:",lines)
 print("Value of D_OurWorld:",D_OurWorld)
 Options.close()
 '''
+'''
 from test9 import Gallons_Of_Beer, Ratio_Of_Malt
-
+'''
 '''
 import HWBeerFunctions
 Options = open("HW_1_Data.txt", "r")
@@ -42,7 +43,52 @@ Grams_Of_Yeast = int(Options.readline().strip())
 
 WORLD = 0
 
-millGrain = HW1BeerFunctions.millGrain()
+millGrain = HW1BeerFunctions_New.millGrain(
+    barleyQuantity = Gallons_Of_Beer * Ratio_Of_Malt,
+    ourWorld = int(WORLD),
+
+)
 
 barleyQuantity = Gallons_Of_Beer * Ratio_Of_Malt
-# I stopped Here
+Pounds_Of_Malt = 0
+
+while True:
+    Pounds_Of_Malt = millGrain(barleyQuantity, WORLD)
+    if Pounds_Of_Malt == 0:
+        print("The Milling Machine Was Broken. Trying Again...")
+    else:
+        break
+mashConversion = HW1BeerFunctions_New.mashConversion(
+    ourWorld = int(WORLD),
+)
+worked = 0
+
+try:
+    worked = mashConversion(0, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been filled!")
+except ValueError:
+    print("Oops, Something Went Wrong!")
+    exit(-1)
+
+try:
+    worked = mashConversion(1, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been heated.")
+except ValueError:
+    print("Oops, Something went wrong!")
+    exit(-1)
+
+try:
+    worked = mashConversion(2, WORLD)
+    if worked == 0:
+        raise ValueError
+    else:
+        print("The tank has been loaded!")
+except ValueError:
+    print("Oops, Something went wrong!")
+    exit(-1)
